79515a88bb49ffcecf3ea0c4feb55d379930bc13
NebukinaVA <sadpool@mail.ru>
2021-11-16 20:56:10 +0300

    fixed local error

--- ODE/ODE/RK3.h
+++ ODE/ODE/RK3.h
@@ -49,7 +49,8 @@ public:
 
 		long double k1 = func(xn, In);
 		long double k2 = func(xn + h / 2.0, In + h * k1 / 2.0);
-		long double k3 = func(xn + h, In + h * (-k1 + 2.0 * k2));
+//		long double k3 = func(xn + h, In + h * (-k1 + 2.0 * k2));
+		long double k3 = func(xn + h, In + h * (2.0 * k2 - k1));
 		In += h * (k1 + 4.0 * k2 + k3) / 6.0;
 		return In;
 	}
@@ -59,7 +60,9 @@ public:
 		arg.push_back(x0);
 		res.push_back(I0);
 		hinc.push_back(0);
+		ss.push_back(0.0);
 		hdec.push_back(0);
+		steps.push_back(h);
 		reshalf.push_back(0.0);
 		long double xn = x0;
 		long double In = I0;
@@ -87,15 +90,15 @@ public:
 				}
 				arg.insert(arg.begin() + i + 1, xn);
 				res.insert(res.begin() + i + 1, In);
-				steps.insert(steps.begin() + i, h);
+				steps.insert(steps.begin() + i + 1, h);
 				exres.insert(exres.begin() + i + 1, ExactSolution(xn));
 				hinc.insert(hinc.begin() + i + 1, 0);
 				hdec.insert(hdec.begin() + i + 1, 0);
 				reshalf.insert(reshalf.begin() + i + 1, 0.0);
+				ss.insert(ss.begin() + i + 1, 0.0);
 				i++;
 			}
 		}
-		steps.push_back(h);
 		return res;
 	}
 	std::vector<long double> calculate_w_error()
@@ -104,13 +107,17 @@ public:
 		ss.push_back(0.0);
 		arg.push_back(x0);
 		res.push_back(I0);
+		steps.push_back(0.0);
+		steps.push_back(h);
 		reshalf.push_back(0.0);
+		hinc.push_back(0);
+		hdec.push_back(0);
 		hinc.push_back(0);
 		hdec.push_back(0);
 		long double xn = x0;
 		long double In = I0;
 		long double xhalf = x0;
-		long double Ihalf;
+		long double Ihalf, reswcap, Inext;
 		long double S;
 		long int i = 0;
 		while (i < n)
@@ -122,7 +129,10 @@ public:
 			else
 			{
 				Ihalf = RK3(xn, In, h / 2.0);
-				S = (RK3(xhalf, Ihalf, h / 2.0) - RK3(xn, In, h)) / 7.0;
+				xhalf = xn + h / 2.0;
+				reswcap = RK3(xhalf, Ihalf, h / 2.0);
+				Inext = RK3(xn, In, h);
+				S = (reswcap - Inext) / 7.0;
 				if ((abs(S) >= (eps / 16.0)) && (abs(S) <= eps))
 				{
 					if ((xn + h) > xmax)
@@ -131,19 +141,19 @@ public:
 						{
 							h /= 2.0;
 						}
-						xhalf += h / 2.0;
 						xn += h;
+						xhalf = xn + h / 2.0;
 						In = RK3(xn, In, h);
-						hinc.insert(hinc.begin() + i + 1, hinc[i]);
-						hdec.insert(hdec.begin() + i + 1, ++(hdec[i]));
+						hinc.insert(hinc.begin() + i + 2, hinc[i+1]);
+						hdec.insert(hdec.begin() + i + 2, ++(hdec[i+1]));
 					}
 					else
 					{
 						In = RK3(xn, In, h);
-						xhalf = xn + h / 2.0;
+					//	xhalf = xn + h / 2.0;
 						xn += h;
-						hinc.insert(hinc.begin() + i + 1, hinc[i]);
-						hdec.insert(hdec.begin() + i + 1, hdec[i]);
+						hinc.insert(hinc.begin() + i + 2, hinc[i+1]);
+						hdec.insert(hdec.begin() + i + 2, hdec[i+1]);
 					}
 
 				}
@@ -155,19 +165,19 @@ public:
 						{
 							h /= 2.0;
 						}
-						xhalf += h / 2.0;
 						xn += h;
+						xhalf = xn + h / 2.0;
 						In = RK3(xn, In, h);
-						hinc.insert(hinc.begin() + i + 1, hinc[i]);
-						hdec.insert(hdec.begin() + i + 1, ++(hdec[i]));
+						hinc.insert(hinc.begin() + i + 2, hinc[i+1]);
+						hdec.insert(hdec.begin() + i + 2, ++(hdec[i+1]));
 					}
 					else {
 						In = RK3(xn, In, h);
-						xhalf = xn + h / 2.0;
+					//	xhalf = xn + h / 2.0;
 						xn += h;
 						h *= 2.0;
-						hinc.insert(hinc.begin() + i + 1, ++(hinc[i]));
-						hdec.insert(hdec.begin() + i + 1, hdec[i]);
+						hinc.insert(hinc.begin() + i + 2, ++(hinc[i+1]));
+						hdec.insert(hdec.begin() + i + 2, hdec[i+1]);
 					}
 				}
 				else if (abs(S) > eps)
@@ -178,46 +188,48 @@ public:
 						{
 							h /= 2.0;
 						}
-						xhalf += h / 2.0;
 						xn += h;
+						xhalf = xn + h / 2.0;
 						In = RK3(xn, In, h);
-						hinc.insert(hinc.begin() + i + 1, hinc[i]);
-						hdec.insert(hdec.begin() + i + 1, ++(hdec[i]));
+						hinc.insert(hinc.begin() + i + 2, hinc[i+1]);
+						hdec.insert(hdec.begin() + i + 2, ++(hdec[i+1]));
 					}
 					else {
 						h = h / 2.0;
 						In = RK3(xn, In, h);
-						xhalf = xn + h / 2.0;
+					//	xhalf = xn + h / 2.0;
 						xn += h;
-						hinc.insert(hinc.begin() + i + 1, hinc[i]);
-						hdec.insert(hdec.begin() + i + 1, ++(hdec[i]));
+						hinc.insert(hinc.begin() + i + 2, hinc[i+1]);
+						hdec.insert(hdec.begin() + i + 2, ++(hdec[i+1]));
 					}
 				}
 				i++;
-				reshalf.insert(reshalf.begin() + i, Ihalf);
-				ss.insert(ss.begin() + i, (S * 8.0));
+				S *= 8.0;
+				reshalf.insert(reshalf.begin() + i, reswcap);
+				ss.insert(ss.begin() + i, S);
 				arg.insert(arg.begin() + i, xn);
 				res.insert(res.begin() + i, In);
-				steps.insert(steps.begin() + (i - 1), h);
+				steps.insert(steps.begin() + i + 1, h);
 				exres.insert(exres.begin() + i, ExactSolution(xn));
 			}		
 		}
-		steps.push_back(h);
 		return res;
 	}
 	long double ExactSolution(long double x)
 	{
-	//	return (E0*(R*sin(w*x) - L * w*cos(w*x) + L * w*exp(-R * x / L)) / (L ^ 2 * w ^ 2 + R ^ 2) + I0 * exp(-R * x / L));
-		return 0;
+		return (E0*(R*sin(w*x) - L * w*cos(w*x) + L * w*exp(-R * x / L)) / (pow(L, 2.0) * pow(w, 2.0) + pow(R, 2.0)) + I0 * exp(-R * x / L));
 	}
 	friend std::ostream & operator<<(std::ostream &out, VC &vc)
 	{
 		if (vc.res.empty())
 			out << "There are no calculated results yet.";
 		else {
+			out << "n  " << " h n-1  " << "    x    " << "      In        " << "        I^        " << "       I      " << "   I - In    " << "       S*      " << "inc " << "dec" << std::endl;
+			long double globerr;
 			for (long int i = 0; i < vc.res.size(); i++)
 			{
-				out << "x" << i << " = " << vc.arg[i] << ",  " << "I" << i << " = " << vc.res[i] << std::endl;
+				globerr = abs(vc.exres[i] - vc.res[i]);
+				out << i << "  " << vc.steps[i] << "      " << vc.arg[i] << "    " << vc.res[i] << "    " << vc.reshalf[i] << "    " << vc.exres[i]  << "    " << globerr << "   " << vc.ss[i] << "    " << vc.hinc[i] << "  " << vc.hdec[i] << std::endl;
 			}
 		}
 		return out;
--- ODE/ODE/main.cpp
+++ ODE/ODE/main.cpp
@@ -3,10 +3,23 @@
 
 int main()
 {
+	/*
+	x0 = _x0;
+		I0 = _I0;
+		L = _L;
+		R = _R;
+		E0 = _E0;
+		w = _w;
+		h = _h;
+		n = _n;
+		eps = _eps;
+		xmax = _xmax;
+		prec = _prec;
+	*/
 	int i;
-	VC equation(0, 1, 2.5, 1.3, 4.2, 2.3, 0.001, 5000, 1e-3, 1.0, 1e-5);
-//	equation.calculate_w_error();
-	equation.calculate();
+	VC equation(0, 0, 0.5, 60.0, 4.0, 6.0, 0.001, 5000, 1e-5, 1.0, 1e-4);
+	equation.calculate_w_error();
+//	equation.calculate();
 	std::cout << "RK3 METHOD\n" << equation << std::endl;
 	std::cin >> i;
 	return 0;
